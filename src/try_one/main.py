8fba02e73d04e59ba766db9b9106205c7f0b7a19
sid <sakha002@ucr.edu>
2025-07-30 15:04:51 -0700

    udpate mport

--- src/try_one/main.py
+++ src/try_one/main.py
@@ -11,8 +11,8 @@ from data.input_parser import InputData
 from data.output_writer import OutputData
 from parameters.intervals import Interval
 from load import Load
-from solar import Solar
-from battery import Battery
+from src.try_one.solar import Solar
+from src.try_one.battery import Battery
 from itc_site import ITCSite
 from asset import Asset
 from energy_import_charge import EnergyImportCharge
